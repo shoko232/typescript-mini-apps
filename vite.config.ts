28161127c03f01dea5483e9410639a5786c9a1b2
Shoko Yuda <shoko_yuda@waku-2.com>
2025-08-28 21:09:31 +0900

    modify: vite.config.tsの修正

--- vite.config.ts
+++ vite.config.ts
@@ -8,7 +8,7 @@ export default defineConfig({
       input: {
         // ここに各HTMLファイルを登録する
         main: resolve(__dirname, 'index.html'),
-        counter: resolve(__dirname, '01-counter.html'),
+        // counter: resolve(__dirname, '01-counter.html'),
         // todoList: resolve(__dirname, '02-todo-list.html'),
         // omikuji: resolve(__dirname, '03-omikuji.html'),
         // calculator: resolve(__dirname, '04-calculator.html'),
